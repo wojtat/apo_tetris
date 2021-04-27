80f8a59eb4de73c38e30ad329f16811eca4d625a
Tilhon, Vojtěch <tilhovoj@fel.cvut.cz>
2021-04-27 23:53:04 +0200

    Initial commit. Hello world

---
+++ main.c
@@ -0,0 +1,22 @@
+/*******************************************************************
+  The entry point of the application
+
+  main.c      - main file
+
+  Author: Vojtech Tilhon, tilhovoj@fel.cvut.cz
+
+ *******************************************************************/
+
+#define _POSIX_C_SOURCE 200112L
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello world\n");
+    return 0;
+}
